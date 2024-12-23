db2ff91cff4a44f5ad0a021e5aef6f54f9bd1d80
Abilash1596 <abil159506@gmail.com>
2024-12-23 12:24:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -4,10 +4,13 @@ import Home from './Home';
 import MainContent from './MainContent';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Signin from './Signin';
+import Layout from './Layout';
 function App() {
   return (
     <div className="App">
+ 
     <BrowserRouter>
+    <Layout/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/signin' element={<Signin/>}/>
